a7b7ea8ffa905cd2a9e000580749b12f3d24f87f
Chan Mee <cmmcme@yunchanmiui-macbook-air.local>
2017-05-02 21:13:25 +0900

    Initial Commit

---
+++ Algorithm_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithm_1
+//
+//  Created by 윤찬미 on 2017. 5. 2..
+//  Copyright © 2017년 윤찬미. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
